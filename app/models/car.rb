13d4b73599af1ea330679eebef4815753c3deabe
Habovskyi <roman.habovskyi.knm.2018@lpnu.ua>
2022-01-24 15:27:10 +0200

    added validation for car

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,10 @@
 class Car < ApplicationRecord
+  validates :make, :model, length: { in: 2..50 }, format: {
+    with: /[a-zA-Z]/,
+    message: 'Please, use english letter!'
+  }, presence: true
+  validates :year, comparison: { greater_than: 1900, less_than_or_equal_to: Date.current.year },
+                   numericality: { only_integer: true }, presence: true
+  validates :price, :odometer, numericality: { only_integer: true }, comparison: { greater_than: 0 }, presence: true
+  validates :description, length: { maximum: 500 }, presence: true
 end
