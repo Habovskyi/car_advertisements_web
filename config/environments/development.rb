99b8b9cec49d5e44149b4a7e32a843a94b580dbb
Habovskyi <roman.habovskyi.knm.2018@lpnu.ua>
2022-01-31 20:12:40 +0200

    fixed rubocop

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,15 +69,4 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   #
-  config.action_mailer.default_options = { from: 'habovskyi.r@gmail.com' }
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    user_name:            'email',
-    password:             'login app',
-    authentication:       'login',
-    enable_starttls_auto: true
-  }
 end
