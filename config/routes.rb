118dd6f94539d5feeaaaa1cc0cea2c968cc433dd
Habovskyi <roman.habovskyi.knm.2018@lpnu.ua>
2022-02-02 21:42:42 +0200

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  resources :cars, only: %i[index show]
-  resources :searches
   root 'homes#index'
 
-  get '/help', to: 'homes#show'
+  resources :cars, only: %i[index show]
+  resources :searches
 
+  get '/help', to: 'homes#show'
   get '/search', to: 'searches#create'
 end
