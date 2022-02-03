91d59ff0906ad781242dae385d53c129bbafbf3e
Habovskyi <roman.habovskyi.knm.2018@lpnu.ua>
2022-02-03 14:30:27 +0200

    add to private search methods

--- app/models/car.rb
+++ app/models/car.rb
@@ -14,14 +14,16 @@ class Car < ApplicationRecord
     sorted_cars.page params[:page]
   end
 
+  private
+
   def search(params)
     result = Car.filter_by_make(params[:make]).filter_by_model(params[:model])
     result = result.filter_by_year_from(params[:year_from]).filter_by_year_to(params[:year_to])
     result.filter_by_price_from(params[:price_from]).filter_by_price_to(params[:price_to])
   end
 
-  scope :filter_by_make, ->(make) { where('make LIKE ?', make) if make.present? }
-  scope :filter_by_model, ->(model) { where('model LIKE ?', model) if model.present? }
+  scope :filter_by_make, ->(make) { where('make ILIKE ?', make) if make.present? }
+  scope :filter_by_model, ->(model) { where('model ILIKE ?', model) if model.present? }
   scope :filter_by_year_from, ->(year_from) { where('year >= ?', year_from) if year_from.present? }
   scope :filter_by_year_to, ->(year_to) { where('year <= ? ', year_to) if year_to.present? }
   scope :filter_by_price_from, ->(price_from) { where('price >= ?', price_from) if price_from.present? }
