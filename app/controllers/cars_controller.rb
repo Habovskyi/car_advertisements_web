3a765064c38074323e4ac015c01e6a96fe170ca9
Habovskyi <roman.habovskyi.knm.2018@lpnu.ua>
2022-02-03 12:31:00 +0200

    refactoring search

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,6 +1,6 @@
 class CarsController < ApplicationController
   def index
-    @cars = FindCar.new.call(search_params)
+    @cars = Car.new.call(search_params)
   end
 
   def show
--- app/models/car.rb
+++ app/models/car.rb
@@ -7,4 +7,22 @@ class Car < ApplicationRecord
                    numericality: { only_integer: true }, presence: true
   validates :price, :odometer, numericality: { only_integer: true }, comparison: { greater_than: 0 }, presence: true
   validates :description, length: { maximum: 500 }, presence: true
+
+  def call(params)
+    search(params).sorter(params[:sort_type] ||= :created_at, params[:sort_direction] ||= :desc).page params[:page]
+  end
+
+  def search(params)
+    result = Car.make(params[:make]).mode(params[:model])
+    result = result.year_from(params[:year_from]).year_to(params[:year_to])
+    result.price_from(params[:price_from]).price_to(params[:price_to])
+  end
+
+  scope :make, ->(make) { where('make LIKE ?', make) if make.present? }
+  scope :mode, ->(model) { where('model LIKE ?', model) if model.present? }
+  scope :year_from, ->(year_from) { where('year >= ?', year_from) if year_from.present? }
+  scope :year_to, ->(year_to) { where('year <= ? ', year_to) if year_to.present? }
+  scope :price_from, ->(price_from) { where('price >= ?', price_from) if price_from.present? }
+  scope :price_to, ->(price_to) { where('price <= ?', price_to) if price_to.present? }
+  scope :sorter, ->(type, direction) { order(type => direction) }
 end
