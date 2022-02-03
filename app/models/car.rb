9a41991c498faa8bf7723a06b8bd0436625729b0
Habovskyi <roman.habovskyi.knm.2018@lpnu.ua>
2022-02-03 13:19:03 +0200

    refactoring search #2

--- app/models/car.rb
+++ app/models/car.rb
@@ -9,20 +9,22 @@ class Car < ApplicationRecord
   validates :description, length: { maximum: 500 }, presence: true
 
   def call(params)
-    search(params).sorter(params[:sort_type] ||= :created_at, params[:sort_direction] ||= :desc).page params[:page]
+    searched_cars = search(params)
+    sorted_cars = searched_cars.sorter(params[:sort_type] ||= :created_at, params[:sort_direction] ||= :desc)
+    sorted_cars.page params[:page]
   end
 
   def search(params)
-    result = Car.make(params[:make]).mode(params[:model])
-    result = result.year_from(params[:year_from]).year_to(params[:year_to])
-    result.price_from(params[:price_from]).price_to(params[:price_to])
+    result = Car.filter_by_make(params[:make]).filter_by_model(params[:model])
+    result = result.filter_by_year_from(params[:year_from]).filter_by_year_to(params[:year_to])
+    result.filter_by_price_from(params[:price_from]).filter_by_price_to(params[:price_to])
   end
 
-  scope :make, ->(make) { where('make LIKE ?', make) if make.present? }
-  scope :mode, ->(model) { where('model LIKE ?', model) if model.present? }
-  scope :year_from, ->(year_from) { where('year >= ?', year_from) if year_from.present? }
-  scope :year_to, ->(year_to) { where('year <= ? ', year_to) if year_to.present? }
-  scope :price_from, ->(price_from) { where('price >= ?', price_from) if price_from.present? }
-  scope :price_to, ->(price_to) { where('price <= ?', price_to) if price_to.present? }
+  scope :filter_by_make, ->(make) { where('make LIKE ?', make) if make.present? }
+  scope :filter_by_model, ->(model) { where('model LIKE ?', model) if model.present? }
+  scope :filter_by_year_from, ->(year_from) { where('year >= ?', year_from) if year_from.present? }
+  scope :filter_by_year_to, ->(year_to) { where('year <= ? ', year_to) if year_to.present? }
+  scope :filter_by_price_from, ->(price_from) { where('price >= ?', price_from) if price_from.present? }
+  scope :filter_by_price_to, ->(price_to) { where('price <= ?', price_to) if price_to.present? }
   scope :sorter, ->(type, direction) { order(type => direction) }
 end
