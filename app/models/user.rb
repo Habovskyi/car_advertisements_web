a9232f15c73017a520f506e53f4eaf9389240cdc
Habovskyi <roman.habovskyi.knm.2018@lpnu.ua>
2022-02-10 11:47:57 +0200

    delete coments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :searches, dependent: :delete_all
 
   devise :database_authenticatable, :registerable,
