baf3ed8d22ff388111801e629ae0770f836f24ec
Habovskyi <roman.habovskyi.knm.2018@lpnu.ua>
2022-02-10 10:47:17 +0200

    remove scope from private

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,7 +3,7 @@ class Car < ApplicationRecord
     with: /[a-zA-Z0-9\-]/,
     message: 'Please, use english letter!'
   }, presence: true
-  validates :year, comparison: { greater_than: 1900, less_than_or_equal_to: Date.current.year },
+  validates :year, comparison: { greater_than_or_equal_to: 1900, less_than_or_equal_to: Date.current.year },
                    numericality: { only_integer: true }, presence: true
   validates :price, :odometer, numericality: { only_integer: true }, comparison: { greater_than: 0 }, presence: true
   validates :description, length: { maximum: 500 }, presence: true
@@ -13,14 +13,6 @@ class Car < ApplicationRecord
     searched_cars.sorter(params[:sort_type] ||= :created_at, params[:sort_direction] ||= :desc)
   end
 
-  private
-
-  def search(params)
-    result = Car.filter_by_make(params[:make]).filter_by_model(params[:model])
-    result = result.filter_by_year_from(params[:year_from]).filter_by_year_to(params[:year_to])
-    result.filter_by_price_from(params[:price_from]).filter_by_price_to(params[:price_to])
-  end
-
   scope :filter_by_make, ->(make) { where('make ILIKE ?', make) if make.present? }
   scope :filter_by_model, ->(model) { where('model ILIKE ?', model) if model.present? }
   scope :filter_by_year_from, ->(year_from) { where('year >= ?', year_from) if year_from.present? }
@@ -28,4 +20,12 @@ class Car < ApplicationRecord
   scope :filter_by_price_from, ->(price_from) { where('price >= ?', price_from) if price_from.present? }
   scope :filter_by_price_to, ->(price_to) { where('price <= ?', price_to) if price_to.present? }
   scope :sorter, ->(type, direction) { order(type => direction) }
+
+  private
+
+  def search(params)
+    result = Car.filter_by_make(params[:make]).filter_by_model(params[:model])
+    result = result.filter_by_year_from(params[:year_from]).filter_by_year_to(params[:year_to])
+    result.filter_by_price_from(params[:price_from]).filter_by_price_to(params[:price_to])
+  end
 end
