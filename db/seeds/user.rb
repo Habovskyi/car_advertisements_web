c75a13fa77f4ad7d2b07f39ef3a6c939baaac74a
Habovskyi <roman.habovskyi.knm.2018@lpnu.ua>
2022-02-12 01:20:13 +0200

    change search seeds

--- db/seeds/user.rb
+++ db/seeds/user.rb
@@ -1,8 +1,8 @@
-User.create(email: 'user1@gmail.com', password: 'testuser')
-User.create(email: 'user2@gmail.com', password: 'testuser')
-User.create(email: 'user3@gmail.com', password: 'testuser')
+user_one = User.create!(email: 'user1@gmail.com', password: 'testuser')
+user_two = User.create!(email: 'user2@gmail.com', password: 'testuser')
+user_three = User.create!(email: 'user3@gmail.com', password: 'testuser')
 
-def create_search(id)
+def create_search(current_user)
   Search.create(
     make: FFaker::Vehicle.make,
     model: FFaker::Vehicle.model,
@@ -10,10 +10,10 @@ def create_search(id)
     year_to: FFaker::Vehicle.year,
     price_from: FFaker::Number.rand(10..1000) * 100,
     price_to: FFaker::Number.rand(10..1000) * 100,
-    user_id: id
+    user_id: current_user.id
   )
 end
 
-50.times { create_search(User.last.id - 2) }
-50.times { create_search(User.last.id - 1) }
-50.times { create_search(User.last.id) }
+50.times { create_search(user_one) }
+50.times { create_search(user_two) }
+50.times { create_search(user_three) }
